ebd7a9ea53fd939d16a38b19666ad23a20493da8
Andrew Horn <arhorn@smcm.edu>
2020-04-01 13:44:57 -0500

    added advanced version

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Input,
   InputGroup,
   InputGroupAddon,
+  Button,
 } from "reactstrap";
 import roundTo from "round-to";
 import "./App.css";
@@ -16,16 +17,31 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      advancedVersion: false,
       billAmount: "",
       numberOfPeople: "",
+      firstPayPercent: "",
+      firstPersonAmount: 0,
     };
+    this.toggleAdvanced = this.toggleAdvanced.bind(this);
     this.handleBillChange = this.handleBillChange.bind(this);
     this.handlePeopleChange = this.handlePeopleChange.bind(this);
+    this.handleFirstPayPercentChange = this.handleFirstPayPercentChange.bind(
+      this
+    );
+  }
+  toggleAdvanced() {
+    const advancedVersion = !this.state.advancedVersion;
+    this.setState({ advancedVersion });
   }
   handleBillChange(e) {
     const billAmount = e.target.value;
     if (billAmount === "" || /^\d*\.?\d{0,2}$/.test(billAmount)) {
-      this.setState({ billAmount });
+      const firstPayAmount = roundTo(
+        this.state.firstPayPercent * 0.01 * this.state.billAmount,
+        2
+      );
+      this.setState({ billAmount, firstPayAmount });
     }
   }
   handlePeopleChange(e) {
@@ -34,16 +50,31 @@ class App extends Component {
       this.setState({ numberOfPeople });
     }
   }
-  render() {
-    const calculateFinalAmount = () => {
-      const finalAmount = roundTo(
-        this.state.billAmount / this.state.numberOfPeople,
+  handleFirstPayPercentChange(e) {
+    const firstPayPercent = e.target.value;
+    if (firstPayPercent === "" || /^\d{0,2}$/.test(firstPayPercent)) {
+      const firstPayAmount = roundTo(
+        firstPayPercent * 0.01 * this.state.billAmount,
         2
       );
-      if (!finalAmount) {
-        return "";
+      this.setState({ firstPayPercent, firstPayAmount });
+    }
+  }
+  render() {
+    const calculateFinalAmount = () => {
+      if (!this.state.advancedVersion) {
+        return (
+          "$" + roundTo(this.state.billAmount / this.state.numberOfPeople, 2)
+        );
       }
-      return "$" + finalAmount;
+      return (
+        "$" +
+        roundTo(
+          (this.state.billAmount - this.state.firstPayAmount) /
+            (this.state.numberOfPeople - 1),
+          2
+        )
+      );
     };
     return (
       <div>
@@ -53,6 +84,13 @@ class App extends Component {
             <Col xs="12" sm="6">
               <h1>Bill Splitter</h1>
               <h4>created by Andrew Horn</h4>
+              <br />
+              <br />
+              <Button color="primary" onClick={this.toggleAdvanced}>
+                {this.state.advancedVersion
+                  ? "Advanced Version"
+                  : "Basic Version"}
+              </Button>
               <br />
               <br />
               <Form>
@@ -73,12 +111,43 @@ class App extends Component {
                   onChange={this.handlePeopleChange}
                 />
                 <br />
-                <br />
-                <br />
-                {this.state.billAmount && this.state.numberOfPeople && (
-                  <p>Each should pay</p>
+                {this.state.advancedVersion && (
+                  <div>
+                    <Label>Percent 1st person pays</Label>
+                    <InputGroup>
+                      <Input
+                        type="text"
+                        value={this.state.firstPayPercent}
+                        onChange={this.handleFirstPayPercentChange}
+                      />
+                      <InputGroupAddon addonType="append">%</InputGroupAddon>
+                    </InputGroup>
+                  </div>
                 )}
-                <p id="final-amount">{calculateFinalAmount()}</p>
+                <br />
+                <br />
+                <br />
+                {this.state.billAmount &&
+                  this.state.numberOfPeople &&
+                  (!this.state.advancedVersion ||
+                    this.state.firstPayPercent) && (
+                    <div>
+                      {this.state.advancedVersion && (
+                        <div>
+                          <p>First person should pay</p>
+                          <p className="final-amount">
+                            {"$" + this.state.firstPayAmount}
+                          </p>
+                          <br />
+                        </div>
+                      )}
+                      <p>
+                        Each {this.state.advancedVersion && "other person"}{" "}
+                        should pay
+                      </p>
+                      <p className="final-amount">{calculateFinalAmount()}</p>
+                    </div>
+                  )}
               </Form>
             </Col>
           </Row>
