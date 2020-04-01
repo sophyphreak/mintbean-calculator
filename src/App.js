adcdb88f3f6c3f43788cadf825d3880fb9e7db46
Andrew Horn <arhorn@smcm.edu>
2020-04-01 12:21:54 -0500

    build basic version, currently not working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import React, { Component } from "react";
 import { Container, Row, Col, Form, Label, Input } from "reactstrap";
 import "./App.css";
 
+const finalAmountStyle = {
+  fontSize: "2em",
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      billAmount: "",
+      numberOfPeople: "",
+    };
+    this.handleBillChange.bind(this);
+    this.handlePeopleChange.bind(this);
+  }
+  handleBillChange(e) {
+    this.setState({ billAmount: e.target.value });
+  }
+  handlePeopleChange(e) {
+    this.setState({ numberOfPeople: e.target.value });
+  }
   render() {
+    const finalAmount = () =>
+      this.state.billAmount / this.state.numberOfPeople || "";
     return (
       <Container>
-        <Form>
-          <Label>Bill Amount</Label>
-          <Input type="text" />
-        </Form>
+        <Row>
+          <Col xs="0" sm="3" md="3"></Col>
+          <Col xs="12" sm="6">
+            <br />
+            <br />
+            <Form>
+              <Label>Bill Amount</Label>
+              <Input type="text" />
+              <br />
+              <Label>Number of people</Label>
+              <Input type="text" />
+              <br />
+              <p>Amount each should pay</p>
+              <p style={finalAmountStyle}>{finalAmount()}</p>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     );
   }
